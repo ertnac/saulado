742f196d7c8c981ba0616361fd4d910da768a4fb
ertnac <jamesclarencecantre@gmail.com>
2026-03-28 02:17:00 +0800

    vercel deployment v2.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import * as pdfjs from "pdfjs-dist";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const API_URL = '/api';
+const API_URL = '';
 
 function App() {
   // --- CORE STATE ---
